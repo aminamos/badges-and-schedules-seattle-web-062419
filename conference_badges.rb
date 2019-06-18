07a6e4a23edcb8ea635ca8b31f0d1004d49d59fc
Amin Amos <aminamosahs@gmail.com>
2019-06-18 03:17:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(names)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each {|x| puts}
+  batch_badge_creator(attendees).each {|x| puts x}
   # puts assign_rooms(attendees[0])
   # assign_rooms(attendees).each {|x| << attendees}
   
